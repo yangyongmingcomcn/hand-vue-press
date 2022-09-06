7181896780d5eae424f0af2c02c2215f7b7bd64d
yangyongming <yongming.yang@hand-china.com>
2022-09-06 14:19:25 +0800

    feat: 新增自动生成navbar插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,26 +1,12 @@
-const moment = require("moment");
 const navConf = require('./config/navConf.js');
-const sidebarConf = require('./config/sidebarConf.js');
+const pluginConf = require('./config/pluginConf.js');
 
 module.exports = {
   title: "智协供应链交付规范文档",
   base: "/hand-vue-press/",
-  plugins: {
-    '@vuepress/back-to-top': true,
-    "@vuepress/last-updated": {
-      transformer: (timestamp) => {
-        moment.locale("zh-cn");
-        return moment(timestamp).format("LLLL");
-      },
-    },
-  },
+  plugins: pluginConf,
   themeConfig: {
     lastUpdated: "更新时间",
     nav: navConf,
-    sidebar: sidebarConf
-    // sidebar: {
-    //   "/front/": ["develop_standard", "cux_standard"],
-    //   "/back/": ["develop_standard", "cux_standard"],
-    // },
   },
 };
---
+++ docs/.vuepress/config/pluginConf.js
@@ -0,0 +1,12 @@
+const moment = require("moment");
+
+module.exports = {
+  "@vuepress/back-to-top": true,
+  "@vuepress/last-updated": {
+    transformer: (timestamp) => {
+      moment.locale("zh-cn");
+      return moment(timestamp).format("LLLL");
+    },
+  },
+  "vuepress-plugin-auto-sidebar": {},
+};
